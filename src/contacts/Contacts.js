371a6ad6e727c72ffaae6eeab501fb113c8d4b4d
Gepiro <gepiro0916@gmail.com>
2023-03-14 12:16:05 +0100

    Sandro: Immagine homepage e Contact form

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -4,8 +4,24 @@ import Navbar from "../navbar/Navbar";
 import Footer from "../footer/Footer";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
+import axios from 'axios';
 
 const Contacts = () => {
+
+    function sendMail(event){
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        axios.post('https://formsubmit.co/younginfolife@gmail.com', {
+            Email: data.get('email'),
+            Testo: data.get('message')
+        }).then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    }
+
 
     return (
         <Box>
@@ -14,13 +30,23 @@ const Contacts = () => {
 
             <Box>
 
-                <Container component="main" maxWidth="xs">
+                <Container component="main" maxWidth="sm">
                     <h1>Contattaci</h1>
+
                     <Card sx={{
-                        padding: '7%',
-                        paddingLeft: '13%',
-                        paddingRight: '13%',
-                        marginTop: '15%',
+                        padding: '3%',
+                        textAlign: 'center',
+                        border: '3px solid',
+                        borderColor: '#EEEEEE',
+                        borderRadius: '12%'
+                    }}>
+                        <p>Compilare questo <a href={"https://docs.google.com/forms/d/e/1FAIpQLScnU4WZapdWtZn5Bl6r6adntj8XL7MRhsWFyfsCKNon72xxmQ/viewform?usp=sf_link"}>form</a> per iscriverti al gruppo.</p>
+                    </Card>
+
+                    <h3>Scrivici un messaggio</h3>
+
+                    <Card sx={{
+                        padding: '5%',
                         border: '3px solid',
                         borderColor: '#EEEEEE',
                         borderRadius: '5%'
@@ -28,7 +54,7 @@ const Contacts = () => {
                         <Box sx={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center',
                         }}>
-                            <Box component="form" noValidate sx={{mt: 1}}>
+                            <Box component="form" onSubmit={sendMail} noValidate sx={{mt: 1}}>
                                 <TextField
                                     margin="normal"
                                     required
@@ -59,17 +85,7 @@ const Contacts = () => {
                             </Box>
                         </Box>
                     </Card>
-                    <Card sx={{
-                        padding: '7%',
-                        paddingLeft: '13%',
-                        paddingRight: '13%',
-                        marginTop: '15%',
-                        border: '3px solid',
-                        borderColor: '#EEEEEE',
-                        borderRadius: '5%'
-                    }}>
-                        <p>Compilare questo form per iscriverti al gruppo.</p>
-                    </Card>
+
                 </Container>
             </Box>
 
--- src/homepage/HomePage.js
+++ src/homepage/HomePage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from "../navbar/Navbar";
 import Footer from "../footer/Footer";
 import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import slide from '../images/HomePageSlide.png'
 
 function HomePage() {
     return (
@@ -10,10 +12,17 @@ function HomePage() {
             <Navbar/>
 
             <Box>
-                <h1>Chi Siamo?</h1>
-                <p>
-                    Il laboratorio nazionale InfoLife del CINI (Consorzio Interuniversitario Nazionale di Informatica) promuove iniziative di networking tra ricercatori di formazione informatica che conducono ricerche rilevanti in bioinformatica e tematiche correlate, insieme ai loro partner internazionali. Il gruppo Young InfoLife …..
-                </p>
+
+                <Box maxWidth={"xs"}>
+                    <img  style={{ width: "100%", height: 150 }} src={slide}/>
+                </Box>
+
+                <Container component="main" maxWidth="sm">
+                    <h1>Chi Siamo?</h1>
+                    <p>
+                        Il laboratorio nazionale InfoLife del CINI (Consorzio Interuniversitario Nazionale di Informatica) promuove iniziative di networking tra ricercatori di formazione informatica che conducono ricerche rilevanti in bioinformatica e tematiche correlate, insieme ai loro partner internazionali. Il gruppo Young InfoLife …..
+                    </p>
+                </Container>
             </Box>
 
             <Footer/>
