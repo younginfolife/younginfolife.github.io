13553eaf020bd58ca3a902851c9f22feb4100c75
Gepiro <gepiro0916@gmail.com>
2023-04-14 16:10:24 +0200

    Sandro: Aggiunti Eventi

--- src/homepage/HomePage.js
+++ src/homepage/HomePage.js
@@ -37,12 +37,15 @@ function HomePage() {
                         <h1>Chi Siamo?</h1>
                         Il gruppo CINI Young-InfoLife fornisce una piattaforma per gli studenti, dottorandi e giovani ricercatori nel campo dell'informatica, che sono appassionati e coinvolti nelle tematiche trattate dal laboratorio nazionale InfoLife del CINI (Consorzio Interuniversitario Nazionale di Informatica). 
                         CINI Young-InfoLife è gestito da giovani (bio)informatici per giovani (bio)informatici, e promuove il networking e la formazione dei propri affiliati attraverso diverse azioni che includono l’organizzazione di eventi/conferenze, l’erogazione di corsi/seminari, la pubblicazione di offerte/opportunità (tesi, progetti, conferenze, posizioni lavorative, ecc.), la divulgazione di contenuti scientifici, ecc.
-
-                        In un campo fortemente interdisciplinare come la bioinformatica, CINI Young-InfoLife si distingue da iniziative simili per la forte impronta metodologica e informatica e, pur rimanendo aperto agli essenziali aspetti più applicativi e biologici, vuole concentrarsi sulle metodologie informatiche e sul loro ruolo nell’ambito delle scienze della vita. 
-
-                        CINI Young-InfoLife vuole essere un piccolo ma dinamico tassello nel più ampio mosaico che è il CINI, e dare un importante contributo nelle attività del CINI, del laboratorio InfoLife e della bioinformatica in Italia.
-
-                        Se sei un giovane bioinformatico seguici sui nostri canali social …Oppure richiedi la tua partecipazione tramite il form sul nostro sito web.
+                        <br/>
+                        <br/>
+                        In un campo fortemente interdisciplinare come la bioinformatica, CINI Young-InfoLife si distingue da iniziative simili per la forte impronta metodologica e informatica e, pur rimanendo aperto agli essenziali aspetti più applicativi e biologici, vuole concentrarsi sulle metodologie informatiche e sul loro ruolo nell’ambito delle scienze della vita.
+                        <br/>
+                        <br/>
+                        CINI Young-InfoLife vuole essere un piccolo ma dinamico tassello nel più ampio mosaico che è il CINI, e dare un importante contributo nelle attività del CINI, del laboratorio InfoLife e della bioinformatica in Italia
+                        <br/>
+                        <br/>
+                        Se sei un giovane bioinformatico seguici sui nostri canali social oppure richiedi la tua partecipazione tramite il form sul nostro sito web.
                         Altrimenti, contattaci per promuovere le tue attività, proposte di formazione o lavorative ai membri del nostro gruppo.
 
                     </Box>
