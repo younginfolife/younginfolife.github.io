5ae669a8e4ea32b2707b2d0cd98cca09c73b3e3a
Nicola <nicola.licheri@gmail.com>
2023-05-31 17:07:54 +0200

    Nicola: update mappa

--- src/team/Team.js
+++ src/team/Team.js
@@ -1,105 +1,137 @@
-import {React, useState, useRef} from 'react'
-import {Box} from "@mui/material";
+import {React, useState, useRef, useEffect} from 'react'
+// import {Box} from "@mui/material";
+import {Box, Card, Grid} from "@mui/material";
 import Navbar from "../navbar/Navbar";
 import Footer from "../footer/Footer";
 import './Team.css'
 import Container from "@mui/material/Container";
 // import map from "../images/mappa.png";
-import {MapContainer, useMap} from 'react-leaflet'
-import L from 'leaflet';
+import {MapContainer, useMap, GeoJSON, TileLayer} from 'react-leaflet'
+import L, { marker, LatLngExpression } from 'leaflet';
 import 'leaflet.markercluster'
+
 
 import {member_list} from "./members"
 
 
 
 const MapComponent = () => {
-    
 
-    const LoadPoints = ( map ) => {
+    const InitMap = () => {
+        const italyBounds = L.latLngBounds(
+            L.latLng(35.29, 6.62), // Southwest coordinates of Italy
+            L.latLng(47.09, 18.78) // Northeast coordinates of Italy
+        );
+        const map = useMap(); 
+
+        map.setZoom(6);
+        map.panTo([41.9, 12.5]); 
+        map.setMaxBounds(italyBounds);
+        map.bounds=italyBounds;
+        map.scrollWheelZoom=true;
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);     
+
+        PutPeopleOnMap( map ); 
+    }
+
+
+    const PutPeopleOnMap = ( map ) => {
         var markers = new L.markerClusterGroup({
             spiderfyOnMaxZoom: true,
             showCoverageOnHover: false,
-            zoomToBoundsOnClick: true
+            zoomToBoundsOnClick: true, 
+            // maxClusterRadius: 80
+            singleMarkerMode: true
         });
 
-
-        LoadDataPoints().forEach(( coord ) => {
-            markers.addLayer( 
-                new L.CircleMarker( coord.slice(2, 4), {
-                    title: coord.at(1),
-                    radius: 5,
-                    // color: '#FFFFFF',
-                    weight: 1, //coord.at(2),
-                    fillOpacity: 0.5,
-                    fillColor: 'green'
-                } ).bindPopup( coord.at(0)  )
+        
+        LoadDataPoints().forEach(( record ) => {
+            markers.addLayer(
+                new L.CircleMarker(
+                    L.latLng(record.latitude, record.longitude), {
+                        title: record.citta, 
+                        radius: 5, 
+                        weight: 1, 
+                        fillOpacity: 0.5, 
+                        fillColor: "green"
+                    }
+                ).bindPopup( GetPopup( record ) )
             );
         });
 
         map.addLayer( markers );
     }
 
+    
+
+
+    const GetPopup = ( record ) => {
+        return  "<h3>"+record.nome + " " + record.cognome+"</h3>";
+    }
+
 
     const LoadDataPoints = () => {
-        //TODO: load data from excel file 
-        const city_coordinates = member_list;
- 
-        return city_coordinates;
+        return member_list;
     }
 
-
-    const InitMap = () => {
-        const map = useMap(); 
-
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);     
-
-        LoadPoints( map ); 
-    }
-
-
-    const defaultPosition = [ 41.902782, 12.496366]; //Rome coordinates
     return (
-        <MapContainer center={defaultPosition} style={{height:666}} zoom={6} scrollWheelZoom={true}>
+        <MapContainer style={{height:777, width:777}}>
             <InitMap/>
         </MapContainer>
     );
 }
 
 
-
-
-
 const Team = () => {
-
     return (
-
         <Box>
-
             <Navbar/>
-
-            <Box color={"white"}>
-                <Container component="main" maxWidth="lg">
-                    <h1>TEAM</h1>
-                    {/* <img src={map} id={"Map"}/> */}
-                <MapComponent/>
-                <h2>Direttivo:</h2>
-                <ul>
-                    <li>Giacomo Baruzzo: giacomo.baruzzo@unipd.it</li>
-                    <li>Vincenzo Bonnici: vincenzo.bonnici@unipr.it </li>
-                    <li>Simone Pernice: simone.pernice@unito.it</li>
-                </ul>
-
-                {/* responsabile di area di servizio */}
-                
-                </Container>
-            </Box>
-
-            <Footer/>a
+            <Container component="main" maxWidth="lg">
+                <Grid container spacing={3} sx={{paddingTop:"2%"}}>
+                    <Grid>
+                        <MapComponent/>
+                    </Grid>
+                    <Grid item xs={12} md={7} lg={4} color={"red"} width={"100%"}>
+                        <Card sx={{
+                            padding: '5%',
+                            border: '3px solid',
+                            borderColor: '#EEEEEE',
+                            borderRadius: '5%', 
+                            // backgroundColor: "red"
+                        }}>
+                            <h2>Direttivo</h2>
+                            <ul>
+                                <li>Giacomo Baruzzo: giacomo.baruzzo@unipd.it</li>
+                                <li>Vincenzo Bonnici: vincenzo.bonnici@unipr.it </li>
+                                <li>Simone Pernice: simone.pernice@unito.it</li>
+                            </ul>
+                        </Card>
+                        <Card sx={{
+                            marginTop: "5%",
+                            padding: '10%',
+                            border: '3px solid',
+                            borderColor: '#EEEEEE',
+                            borderRadius: '5%'
+                        }}>
+                            <h2>Social media managers</h2>
+                            <ul>
+                                <li>Mikele Milia: Facebook</li>
+                                <li>Alessio Funari: LinkedIn</li>
+                                <li>Eva Viesi: Telegram</li>
+                                <li>Pasquale Sibilio: Twitter</li>
+                                <li>Salvatore Calderaro: Youtube</li> 
+                            </ul>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Container>
+            <Footer/>
         </Box>
-    )
+    );
 }
+
+
 export default Team
--- src/team/converti_coordinate.py
+++ src/team/converti_coordinate.py
@@ -4,11 +4,70 @@ import argparse
 import pandas as pd 
 from geopy.geocoders import Nominatim
 
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+import os, logging
+
+
+class SpreadsheetManager:
+    # If modifying these scopes, delete the file token.json.
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+    def __init__(self) -> None:
+        creds = None
+
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SpreadsheetManager.SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file( 'credentials.json', SpreadsheetManager.SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+
+        self.__creds = creds 
+
+
+
+    def get_spreadsheet(self, spreadsheet_id: str, sheet_id: str) -> pd.DataFrame:
+        try:
+            service = build('sheets', 'v4', credentials=self.__creds)
+
+            # Call the Sheets API
+            sheet = service.spreadsheets()
+            result = sheet.values().get(spreadsheetId=spreadsheet_id,
+                                        range=sheet_id).execute()
+            
+            if (values := result.get('values', [])) is False:
+                print('No data found.')
+                return
+
+            columns = values.pop(0) 
+            data = [ row for row in values ]
+
+            df = pd.DataFrame( data = data, columns = columns )
+            return df 
+
+        except HttpError as err:
+            print(err) 
+
+
 
 class CoordinateManager:
-    def __init__(self) -> None:
+    def __init__(self, city_coords_map: dict = None) -> None:
         self.__geolocator = Nominatim(user_agent="SIMONE PERNICE RTDA IL SUPREMO")
-        self.__coordinates = dict() 
+        self.__coordinates = city_coords_map.copy() if city_coords_map else dict() 
 
     def get_coordinates(self, cityname: str):
         """ Given a city name, returns its coordinates: (latitude, longitude) """
@@ -20,66 +79,55 @@ class CoordinateManager:
         self.__coordinates[ cityname ] = coordinates = (location.latitude, location.longitude )
         return coordinates
 
-    def initialize(self, city_coords_map: dict):
-        """ Initialize stuff"""
-
-        self.__coordinates = city_coords_map.copy() 
-
 
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input_file", type=str, required=True)
-    parser.add_argument("-o", "--output_file", type=str, required=True)
+    # parser.add_argument("-i", "--input_file", type=str, required=True)
+    # parser.add_argument("-o", "--output_file", type=str, required=True)
     parser.add_argument("--js", type=str, default="members.js")
-    parser.add_argument("--geolocate", action="store_true")
+    # parser.add_argument("--geolocate", action="store_true")
     args = parser.parse_args() 
 
+    spreadsheet_id = "1urvnXcliGEyf2roUhnyQQtegYi0GYPmlU9SAj7wasHQ"
+    sheetname = "Form Responses 1"
+
+    df = SpreadsheetManager().get_spreadsheet( spreadsheet_id, sheetname )
+    assert df is not None 
+    df.columns = ["useless", "nome", "cognome", "uni", "citta", "ruolo", "email", "formazione", "ambiti"]
+
+    logging.info(f"Spreadsheet obtained successfully: {df.shape}")
+    
+
+    # if True:#args.geolocate: 
+        # df = pd.read_csv( args.input_file, sep="\t",  header=0, index_col=None )
 
     coordinator = CoordinateManager() 
+    ## get coordinates for each city 
+    coordinates = { city: coordinator.get_coordinates( city ) for city in df.citta.unique() }
+    ## build the city column for each person on the dataframe 
+    coo_col = [ coordinates.get(city) for city in df.citta ]
+    ## extract latitude and longitude from the previous list 
+    lats, longs = zip( * coo_col )
 
+    ## add the new columns to the dataframe 
+    df[ "coords" ] = coo_col
+    df[ "latitude" ] = lats
+    df[ "longitude" ] = longs   
 
-    if args.geolocate: 
-        # geolocator = Nominatim(user_agent="SIMONE PERNICE RTDA IL SUPREMO")
-
-        df = pd.read_csv( args.input_file, sep="\t",  header=0, index_col=None )
-
-        coordinates = { city: coordinator.get_coordinates( city ) for city in df.citta.unique() }
-        coo_col = [ coordinates.get(city) for city in df.citta ]
-
-        lats, longs = zip( * coo_col )
-
-        df[ "coords" ] = coo_col
-        df[ "latitude" ] = lats
-        df[ "longitude" ] = longs
-
-        df.to_csv(args.output_file, sep="\t", index=False, header=True)
-
-
-    df = pd.read_csv( args.input_file, sep="\t", header=0, index_col=None )
-    my_records = list() 
-
-
-    for cityname, subdf in df.groupby( "citta" ):
-        # lat, long = subdf.coords.iloc[0]
-        lat = subdf.latitude.iloc[0]
-        long = subdf.longitude.iloc[0]
-
-        city_records = sorted([
-            [ f"{row.nome} {row.cognome}", cityname, lat, long ]
-                for _, row in subdf.iterrows()
-        ], key=lambda t: t[0].split(" ")[-1])
-
-        my_records.extend( city_records )
-
-
+        ## save the resulting dataframe 
+        # df.to_csv(args.output_file, sep="\t", index=False, header=True)
+    # else:
+    #     df = pd.read_csv( args.input_file, sep="\t", header=0, index_col=None )
+        
+    ## convert dataframe to a list of dictionaries 
+    my_records = df.fillna("").to_dict("records")
+    ## sort record by surname 
+    my_records.sort( key = lambda d: d["cognome"])
 
 
     with open(args.js, "w") as f:
         f.write("export const member_list = ")
         f.write(str(my_records))
-    # print(my_records)
-
-
-    # print(df.columns)
+        
