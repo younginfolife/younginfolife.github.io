8445f8533b3e851f1e1a4ec48c66c1faaaf91f39
Eliseo Martelli <me@eliseomartelli.it>
2024-10-30 09:47:43 +0100

    feat: scaffold landing

---
+++ src/app/page.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={180}
+          height={38}
+          priority
+        />
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://younginfolife.github.io/webpage"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Apri il sito di Young InfoLife →
+        </a>
+      </footer>
+    </div>
+  );
+}
