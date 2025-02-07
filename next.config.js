44e0a7d915ced99f4aebd8c056d5e8bef8c5ad2a
Eliseo Martelli <me@eliseomartelli.it>
2025-02-07 18:19:59 +0100

    fix: Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ import { withContentCollections } from "@content-collections/next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/webpage" : "",
   images: {
     unoptimized: true,
   },
