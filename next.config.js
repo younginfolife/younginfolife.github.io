d369b193e5cabb90cbdf198eaac1b8b807c1028e
Eliseo Martelli <me@eliseomartelli.it>
2025-02-07 09:01:41 +0100

    fix: add unoptimized images

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 import { withContentCollections } from "@content-collections/next";
 
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: "export",
-	basePath: "/webpage",
-	images: {
-		unoptimized: true,
-	}
+  output: "export",
+  //basePath: "/webpage",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withContentCollections(nextConfig);
