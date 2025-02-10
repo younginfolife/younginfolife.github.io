0a82dde269fa47413110d5fec75ffa44cb785eed
Gepiro <gepiro0916@gmail.com>
2025-02-10 10:08:22 +0100

    Added infolife direttivo

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,86 +5,93 @@ import React, { useState } from "react";
 import { allPeople, Person } from "content-collections";
 
 export default function AboutPage() {
-  const [searchTerm, setSearchTerm] = useState<string>("");
-  const [filteredPersons, setFilteredPersons] = useState<Person[]>(allPeople);
+	const [searchTerm, setSearchTerm] = useState<string>("");
+	const [filteredPersons, setFilteredPersons] = useState<Person[]>(allPeople);
 
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const term = event.target.value;
-    setSearchTerm(term);
-    setFilteredPersons(
-      allPeople.filter((person) =>
-        person.name.toLowerCase().includes(term.toLowerCase()),
-      ),
-    );
-  };
+	const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const term = event.target.value;
+		setSearchTerm(term);
+		setFilteredPersons(
+			allPeople.filter((person) =>
+				person.name.toLowerCase().includes(term.toLowerCase()),
+			),
+		);
+	};
 
-  const direttivoPeople = allPeople.filter(
-    (person) => person.occupation === "direttivo",
-  );
-  const smmPeople = allPeople.filter((person) => person.occupation === "smm");
+	const direttivoPeople = allPeople.filter(
+		(person) => person.occupation === "direttivo",
+	);
+	const smmPeople = allPeople.filter((person) => person.occupation === "smm");
 
-  return (
-    <div className="max-w-prose mx-auto flex flex-col gap-8 w-full px-4 md:p-0">
-      <main className="space-y-4">
-        <div className="prose">
-          <h2>Direttivo</h2>
-        </div>
-        <ul className="grid flex-col gap-4 ">
-          {direttivoPeople.map((person, index) => (
-            <li key={index}>
-              <ContactElement {...person} />
-            </li>
-          ))}
-        </ul>
-        <div className="prose">
-          <h2>Gestori Web e Social</h2>
-        </div>
-        <ul className="grid flex-col gap-4 grid-cols-2">
-          {smmPeople.map((person, index) => (
-            <li key={index}>
-              <ContactElement {...person} />
-            </li>
-          ))}
-        </ul>
-        <div className="prose">
-          <h2>La nostra squadra</h2>
-        </div>
-        <div className="relative">
-          <input
-            type="text"
-            placeholder="Nome Cognome"
-            className="px-4 py-2 rounded-full w-full bg-white shadow"
-            value={searchTerm}
-            onChange={handleSearch}
-          />
-          {searchTerm.length > 0 ? (
-            <button
-              className="absolute end-0 px-4 py-2
+	return (
+		<div className="max-w-prose mx-auto flex flex-col gap-8 w-full px-4 md:p-0">
+			<main className="space-y-4">
+				<div className="prose">
+					<h2>Direttivo Infolife</h2>
+				</div>
+				<ul className="grid flex-col gap-4 ">
+					<ContactElement name="Marco Beccuti" location="Università di Torino" />
+					<ContactElement name="Vincenzo Bonnici" location="Università di Parma" />
+				</ul>
+				<div className="prose">
+					<h2>Direttivo Young</h2>
+				</div>
+				<ul className="grid flex-col gap-4 ">
+					{direttivoPeople.map((person, index) => (
+						<li key={index}>
+							<ContactElement {...person} />
+						</li>
+					))}
+				</ul>
+				<div className="prose">
+					<h2>Gestori Web e Social</h2>
+				</div>
+				<ul className="grid flex-col gap-4 grid-cols-2">
+					{smmPeople.map((person, index) => (
+						<li key={index}>
+							<ContactElement {...person} />
+						</li>
+					))}
+				</ul>
+				<div className="prose pt-10">
+					<h2>La nostra squadra Young</h2>
+				</div>
+				<div className="relative">
+					<input
+						type="text"
+						placeholder="Nome Cognome"
+						className="px-4 py-2 rounded-full w-full bg-white shadow"
+						value={searchTerm}
+						onChange={handleSearch}
+					/>
+					{searchTerm.length > 0 ? (
+						<button
+							className="absolute end-0 px-4 py-2
           bg-black text-white font-bold rounded-full"
-              onClick={() => {
-                setSearchTerm("");
-                setFilteredPersons(allPeople);
-              }}
-            >
-              Clear
-            </button>
-          ) : (
-            <></>
-          )}
-        </div>
-        <ul className="grid flex-col gap-4 grid-cols-2">
-          {filteredPersons.map((person, index) => (
-            <li key={index}>
-              <ContactElement {...person} />
-            </li>
-          ))}
-          {filteredPersons.length === 0 ? (
-            <p>No person found matching {searchTerm}.</p>
-          ) : (
-            <></>
-          )}
-        </ul>
-      </main>
-    </div>
-  );
+							onClick={() => {
+								setSearchTerm("");
+								setFilteredPersons(allPeople);
+							}}
+						>
+							Clear
+						</button>
+					) : (
+						<></>
+					)}
+				</div>
+				<ul className="grid flex-col gap-4 grid-cols-2 pb-32">
+					{filteredPersons.map((person, index) => (
+						<li key={index}>
+							<ContactElement {...person} />
+						</li>
+					))}
+					{filteredPersons.length === 0 ? (
+						<p>No person found matching {searchTerm}.</p>
+					) : (
+						<></>
+					)}
+				</ul>
+			</main>
+		</div>
+	);
 }
