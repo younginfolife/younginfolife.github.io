7b6da0c4e2c32c42cf6bb243d57cb0a273e182b7
Gepiro <gepiro0916@gmail.com>
2025-02-11 17:06:55 +0100

    Added biohackathon

---
+++ data/events/2025-biohackathon.md
@@ -0,0 +1,12 @@
+---
+name: BioHackathon 2025
+event: First International BioHackathon of BITS-2025
+description: L’attività BioHackathon, più una serie di webinar a corredo, proposta da CINI Young-Infolife, Young-BITS e iSCB per BITS 2025 ha l’obiettivo di sensibilizzare i partecipanti sull’importanza della riproducibilità nell’analisi dei dati bioinformatici, con un focus sull’RNA-Seq single-cell. 
+long_description: L’attività BioHackathon, più una serie di webinar a corredo, proposta da CINI Young-Infolife, Young-BITS e iSCB per BITS 2025 ha l’obiettivo di sensibilizzare i partecipanti sull’importanza della riproducibilità nell’analisi dei dati bioinformatici, con un focus sull’RNA-Seq single-cell. I webinar saranno degli eventi live, accessibili tramite il canale youtube del gruppo Young-Infolife. Questi mirano alla preparazione dei partecipanti. Il BioHackaton invece, è l’evento proposto durante il meeting annuale della BITS, rappresenta quindi una challenge per sensibilizzare i partecipanti all’uso di strumenti e metodi FAIR per l’analisi di un dataset di Single cell RNA-seq.
+location: Napoli
+dateStart: 2025-06-10
+---
+
+I webinar saranno degli eventi live, accessibili tramite il canale youtube del gruppo Young-Infolife. Questi mirano alla preparazione dei partecipanti. Il BioHackaton invece, è l’evento proposto durante il meeting annuale della BITS, rappresenta quindi una challenge per sensibilizzare i partecipanti all’uso di strumenti e metodi FAIR per l’analisi di un dataset di Single cell RNA-seq.
+
+![Foto di gruppo](/events/BioHackathon/BioHackathon.png)
--- src/app/events/[eventID]/page.tsx
+++ src/app/events/[eventID]/page.tsx
@@ -5,38 +5,40 @@ import { notFound } from "next/navigation";
 import React from "react";
 
 export async function generateStaticParams() {
-  return allEvents.map((e) => ({
-    eventID: e.id,
-  }));
+	return allEvents.map((e) => ({
+		eventID: e.id,
+	}));
 }
 
 interface EventPageParams {
-  eventID: string;
+	eventID: string;
 }
 
 const EventPage = ({ params }: { params: EventPageParams }) => {
-  const event = allEvents.find((event) => event.id === params.eventID);
-  if (!event) {
-    notFound();
-  }
+	const event = allEvents.find((event) => event.id === params.eventID);
+	if (!event) {
+		notFound();
+	}
 
-  return (
-    <div className="prose mx-auto">
-      <h1>{event.name}</h1>
-      <Card className="prose !space-y-2 p-4 rounded-md mx-auto">
-        <span>
-          {event.dateStart} {event.dateEnd}
-        </span>
-        <h2>{event.name}</h2>
-        <p>
-          {event.description}
-          <br />
-          <Link href={event.website!}>Website</Link>
-        </p>
-      </Card>
-      <div dangerouslySetInnerHTML={{ __html: event.html }} />
-    </div>
-  );
+	return (
+		<div className="prose mx-auto pb-32">
+			<h1>{event.name}</h1>
+			<Card className="prose !space-y-2 p-4 rounded-md mx-auto">
+				<span>
+					{event.dateStart} {event.dateEnd}
+				</span>
+				<h2>{event.name}</h2>
+				<p>
+					{event.description}
+					<br />
+					{event.website !== undefined &&
+						<Link href={event.website}>Website</Link>
+					}
+				</p>
+			</Card>
+			<div dangerouslySetInnerHTML={{ __html: event.html }} />
+		</div>
+	);
 };
 
 export default EventPage;
