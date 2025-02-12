be571e2023d5d0b0775e1e9aa4379fdeb66df3c8
Gepiro <gepiro0916@gmail.com>
2025-02-12 10:05:32 +0100

    Sorted publications

--- src/app/publications/page.tsx
+++ src/app/publications/page.tsx
@@ -1,13 +1,14 @@
 import { Card } from "@/components/ui/card";
 import { allPublications } from "content-collections";
 import React from "react";
+import { allPublicationsSorted } from "./sorted";
 
 const page = () => {
   return (
-    <div className="max-w-prose mx-auto w-full p-4 space-y-2">
+    <div className="max-w-prose mx-auto w-full p-4 space-y-2 pb-32">
       <h1 className="text-3xl font-bold">Publicazioni</h1>
       <ul className="space-y-4 max-w-full">
-        {allPublications.map((e, i) => {
+        {allPublicationsSorted.map((e, i) => {
           return (
             <li key={i}>
               <Card className="prose !space-y-2 p-4 mx-auto">
---
+++ src/app/publications/sorted.ts
@@ -0,0 +1,5 @@
+import { allPublications } from "content-collections";
+
+export const allPublicationsSorted = allPublications.toSorted((a, b) =>
+	b.Year.localeCompare(a.Year)
+);
