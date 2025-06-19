fb4f845ebd1882340d845b61729bb622acf95d20
Simone Pernice <123412798+simopernice@users.noreply.github.com>
2025-06-19 12:19:32 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ interface Highlight {
 }
 
 const images = [
+  "/events/2024BITS-Napoli/IMG_4512.jpg",
+  "/events/2025BioHackathon/12d48d39-1f82-451c-8ce8-067177d64a6e.JPG",
   "/carousel/1.jpg",
   "/carousel/2.jpg",
   "/carousel/3.jpg",
