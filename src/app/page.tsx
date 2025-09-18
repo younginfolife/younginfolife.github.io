f69d8bc769a4afdf4099b1a0cca504d8ba80755a
Simone Pernice <123412798+simopernice@users.noreply.github.com>
2025-09-18 15:07:57 +0200

    Add new image path to images array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ interface Highlight {
 }
 
 const images = [
+  "/events/2025CIBB-Milano/Group_cibb25.png",
   "/events/2024BITS-Napoli/IMG_4787.jpg",
   "/events/2025CIBB-Milano/gruppo.jpeg",
   "/events/2025BioHackathon/12d48d39-1f82-451c-8ce8-067177d64a6e.JPG",
